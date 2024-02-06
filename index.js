ab080cf2f92d9037351d973a928b16dc028ba019
Adhil Salim <bizadhil@gmail.com>
2024-02-06 14:16:09 +0530

    add func - followers/following

--- index.js
+++ index.js
@@ -7,10 +7,65 @@
  * 4. Identify users whom we are not following back.
  */
 
-// get username from command line arguments
-const username = process.argv[2];
+async function main() {
+  // get username from command line arguments
+  const username = process.argv[2];
 
-if (!username) {
-  console.log("Please provide a username.");
-  process.exit();
+  if (!username) {
+    console.log("Please provide a username.");
+    process.exit();
+  }
+
+  // get followers
+  console.log("Followers:");
+  await getFollowing(username);
+
+  // get following
+  console.log("Following:");
+  await getFollowers(username);
+}
+
+main();
+
+async function getFollowing(username) {
+  let page = 1;
+  let following = [];
+  while (true) {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/following?page=${page}`
+    );
+    const data = await response.json();
+    if (data.length === 0) {
+      break;
+    }
+    following = following.concat(data);
+    page++;
+  }
+
+  following.map((user, index) => {
+    console.log(`${index + 1}. ${user.login} - ${user.html_url}`);
+  });
+}
+
+async function getFollowers(username) {
+  let page = 1;
+  let followers = [];
+
+  while (true) {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/followers?page=${page}`
+    );
+    const data = await response.json();
+
+    if (data.length === 0) {
+      break;
+    }
+
+    followers = followers.concat(data);
+    page++;
+  }
+
+  followers.map((user, index) => {
+    console.log(`${index + 1}. ${user.login} - ${user.html_url}`);
+  });
 }
